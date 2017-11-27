e38e4b78403dac8efc8adf6445f873afa2347ffb
Flávio Coutinho <fegemo@gmail.com>
2017-11-26 22:28:16 -0200

    Exercise 2: ajax to fetch movies

--- js/starwars.js
+++ js/starwars.js
@@ -2,3 +2,17 @@
 // Use a Star Wars API ( https://swapi.co/) para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+
+$.ajax('https://swapi.co/api/films/', {
+  dataType: 'json',
+  success: function(resposta) {
+    let $listaFilmes = $('#filmes ul');
+    $listaFilmes.empty();
+
+    resposta.results.forEach(function(filme) {
+      $('<li></li>')
+        .html('Episode ' + filme.episode_id + ': ' + filme.title)
+        .appendTo($listaFilmes);
+    });
+  }
+});
